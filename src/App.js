94162917f35c77103053d2eb81648d8bc2670398
Elze Hamilton <elze.hamilton@gmail.com>
2021-09-05 22:51:38 -0500

    Initial commit: Material UI Treeview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TreeView from "@material-ui/lab/TreeView";
+import TreeItem from "@material-ui/lab/TreeItem";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 
-function App() {
+import { useMatomo } from '@datapunt/matomo-tracker-react'
+
+const getTreeItemsFromData = treeItems => {
+	if (treeItems) {
+  return treeItems.map(treeItemData => {
+    let children = undefined;
+    if (treeItemData.successors && treeItemData.successors.length > 0) {
+      children = getTreeItemsFromData(treeItemData.successors);
+    }
+    return (
+      <TreeItem
+        key={treeItemData.id}
+        nodeId={treeItemData.id}
+        label={treeItemData.item}
+        children={children}		
+      />
+    );
+  });
+	}
+};
+const DataTreeView = ({ treeItems }) => {
+  return (
+    <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {getTreeItemsFromData(treeItems)}
+    </TreeView>
+  );
+};
+
+function reducer(state, action) {
+	console.log(`reducer: action.type = ${action.type}`);
+  switch (action.type) {
+	  case 'setSkills': 
+		let newState = {...state, skills: action.skills };
+		return newState;
+    default:
+      throw new Error();
+  }
+}
+
+export default function App() {
+	
+	const initialState = {skills: []};
+	const [state, dispatch] = React.useReducer(reducer, initialState);
+	const { trackPageView, trackEvent } = useMatomo();
+	trackPageView({
+	  documentTitle: `App component with Material UI Treeview: `, // optional
+	  //href: `http://localhost:3000`, // optional
+	  href: `https://material-ui-treeview-elze.vercel.app`, // optional	  	  
+	});	
+  
+	React.useEffect(() => {    
+		async function getSkills() {
+			const response = await fetch('https://sc2019.skillclusters.com/sc/structskills');
+			const skillsFromServer = await response.json();
+			if (response.status !== 200) {
+				trackEvent({ category: `https://sc2019.skillclusters.com/sc/structskills retrieval error`, action: skillsFromServer.message });
+				throw Error(skillsFromServer.message);
+			}
+			console.log(`getSkills`);
+			trackPageView({
+			  documentTitle: `App component with Material UI Treeview: skills loaded `, // optional
+			  href: `http://localhost:3000`, // optional
+			  //href: `https://zeit-buttons-serverless-elze.vercel.app/primarySkill/${primaryTermDecoded}?sort=${sortParam}`, // optional	  
+			});	 				
+			dispatch({type: 'setSkills', skills: skillsFromServer})
+		}
+		console.log(`useEffect is running`);
+
+		getSkills();
+		return () => {
+			console.log('useEffect return');
+		};
+
+	}, [trackPageView, trackEvent]);	
+	
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />	
+      <DataTreeView treeItems={state.skills} />
     </div>
   );
 }
-
-export default App;
